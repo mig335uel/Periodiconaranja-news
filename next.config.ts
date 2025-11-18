3f24aab3458ff37aff9be673ab22f465484fb005
Miguel Angel <miguelangel.gomez.05@usal.es>
2025-11-18 19:54:39 +0100

    cambios en la configuracion de next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import path from "node:path";
 
 const nextConfig: NextConfig = {
-
+    sassOptions: {
+        // Esto le dice a Sass: "Si no encuentras algo, búscalo en la raíz del proyecto"
+        includePaths: [path.join(__dirname)],
+    },
 };
 
 export default nextConfig;
