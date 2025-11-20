dfad1e6dad245ea72c6ccbeddcefd6b5d07e2427
Miguel Angel <miguelangel.gomez.05@usal.es>
2025-11-20 17:10:44 +0100

    correccion de ruta

--- app/adminPanel/post/edit/[slug]/page.tsx
+++ app/adminPanel/post/edit/[slug]/page.tsx
@@ -2,8 +2,12 @@ import NewsEditor from "@/components/TipTapEditor";
 
 
 
+interface Props{
+    params: {
+        slug: string;
+    }
+}
 
-
-export async function PostEdit({params}: {params: {slug: string}}){
+export default async function PostEdit({params}: Props){
     return <NewsEditor />
 }
