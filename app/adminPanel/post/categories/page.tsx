dcc576445ea2bbe282105d165f9127953a8eeeca
Miguel Angel <miguelangel.gomez.05@usal.es>
2025-11-21 16:08:34 +0100

    creación de la ruta categories

---
+++ app/adminPanel/post/categories/page.tsx
@@ -0,0 +1,12 @@
+import MenuPanel from "@/components/Admin/MenuPanel";
+
+
+
+
+export default async function Categories(){
+    return (
+        <div className="flex">
+            <MenuPanel />
+        </div>
+    );
+}
