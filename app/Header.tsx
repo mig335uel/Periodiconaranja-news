22d70f2676fbb143d45f8d05ebd44963782fbaf1
Miguel Angel <gomezbenitezmiguelangel@outlook.com>
2025-12-06 14:07:53 +0100

    arreglo del menú

--- app/Header.tsx
+++ app/Header.tsx
@@ -167,7 +167,7 @@ const Header: React.FC = () => {
                             Inicio
                         </Link>
                     </li>
-                    <li>
+                    <li className="border-r border-gray-200 last:border-r-0">
                         <Link
                             href="/elecciones"
                             className="no-underline text-gray-700 font-bold uppercase text-sm px-6 py-4 block hover:bg-orange-50 hover:text-orange-600 transition"
