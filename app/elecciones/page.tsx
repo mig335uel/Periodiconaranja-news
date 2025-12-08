47ddbb3458ee74d76006ac493daf976430ebf4ac
Miguel Angel <miguelangel.gomez.05@usal.es>
2025-12-08 15:39:14 +0100

    feat: Add new elections page with Escrutinio widget and metadata.

--- app/elecciones/page.tsx
+++ app/elecciones/page.tsx
@@ -2,7 +2,12 @@ import EscrutinioWidget from "@/components/ui/Escrutinio";
 import Header from "../Header";
 
 
-
+export async function generateMetadata() {
+    return {
+        title: 'Elecciones Extremadura 2025',
+        description: 'Resultados en tiempo real de las elecciones',
+    }
+}
 
 
 
