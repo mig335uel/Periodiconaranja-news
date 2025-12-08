f8133518a2da70bac09fcb8de354612be4977333
Miguel Angel <miguelangel.gomez.05@usal.es>
2025-12-08 16:02:16 +0100

    cambios para agregar solo el escrutinioTotal

--- components/tiptap-node/escrutinio-node/escrutinio-component.tsx
+++ components/tiptap-node/escrutinio-node/escrutinio-component.tsx
@@ -1,5 +1,5 @@
 import { NodeViewWrapper } from "@tiptap/react"
-import EscrutinioWidget from "@/components/ui/Escrutinio"
+import EscrutinioTotal from "@/components/ui/EscrutinioTotal"
 import React from "react"
 
 export const EscrutinioComponent = () => {
@@ -7,7 +7,7 @@ export const EscrutinioComponent = () => {
         <NodeViewWrapper className="escrutinio-node-view">
             {/* We wrap it in a div that handles selection styling provided by TipTap/NodeViewWrapper */}
             <div className="relative border-2 border-transparent hover:border-gray-200 transition-colors rounded-lg overflow-hidden my-4 bg-white">
-                <EscrutinioWidget />
+                <EscrutinioTotal />
                 {/* Overlay to prevent interaction conflicts if necessary, though Escrutinio seems read-only/interactive */}
             </div>
         </NodeViewWrapper>
