8abd90d945a05c31f2a4c002f43282687c61978f
Miguel Angel <gomezbenitezmiguelangel@outlook.com>
2025-12-14 14:37:20 +0100

    feat: Add security headers, disable production source maps, and configure asset prefix and rewrites in Next.js.

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,53 @@ import type { NextConfig } from "next";
 import path from "node:path";
 
 const nextConfig: NextConfig = {
+    poweredByHeader: false,
+    assetPrefix: '/assets',
     sassOptions: {
-        // Esto le dice a Sass: "Si no encuentras algo, búscalo en la raíz del proyecto"
         includePaths: [path.join(__dirname)],
     },
+
+  // Quita source maps del navegador (MUY importante)
+  productionBrowserSourceMaps: false,
+
+  // Reduce trazas internas
+  reactStrictMode: true,
+
+  // Headers de seguridad (mitigación real)
+    async headers() {
+        return [
+        {
+            source: "/(.*)",
+            headers: [
+            {
+                key: "X-Content-Type-Options",
+                value: "nosniff",
+            },
+            {
+                key: "X-Frame-Options",
+                value: "DENY",
+            },
+            {
+                key: "Referrer-Policy",
+                value: "strict-origin-when-cross-origin",
+            },
+            {
+                key: "Permissions-Policy",
+                value: "camera=(), microphone=(), geolocation=()",
+            },
+            ],
+        },
+        ];
+    },
+    async rewrites() {
+        return [
+            {
+                source: "/assets/:path*",
+                destination: "/_next/:path*",
+            },
+        ];
+    }
+
 };
 
 export default nextConfig;
