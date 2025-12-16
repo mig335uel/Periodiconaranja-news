e7810cb2f8c4e3a31ed92e6a6aa78763a5135f43
Miguel Angel <gomezbenitezmiguelangel@outlook.com>
2025-12-16 12:55:51 +0100

    feat: add dynamic slug page with metadata generation

---
+++ app/[...slug]/page.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+
+
+
+interface Props {
+    params: {
+        slug: string[]
+    }
+}
+
+
+
+export async function generateMetadata({ params }: { params: Promise<{ slug: string[] }> }) {};
