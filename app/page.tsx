7587aa63c8a9972ac5f86dcfa923a1194b90e78e
Miguel Angel <miguelangel.gomez.05@usal.es>
2025-12-16 20:27:13 +0100

    fix: eliminar la opción de revalidación en la solicitud de la API de publicaciones

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,23 @@ import { buildCategoryPath } from "@/lib/utils";
 import Link from "next/link";
 import Footer from "@/components/Footer";
 import HeroSlider from "@/components/ui/HeroSlider";
+
+
 function getExcerpt(html: string, length: number): string {
   const text = html.replace(/<[^>]*>/g, "").replace(/&[^;]+;/g, "");
   return text.length > length ? text.substring(0, length) + "..." : text;
 }
 
-export default async function Home() {
-  const response = await fetch("https://periodiconaranja.es/wp-json/wp/v2/posts?per_page=25&_fields=id,date,slug,title,excerpt,author,featured_media,jetpack_featured_media_url,categories,_links,_embedded", { next: { revalidate: 300 } });
+
+
+async function fetchPosts(): Promise<Post[]> {
+  const response = await fetch("https://periodiconaranja.es/wp-json/wp/v2/posts?per_page=25&_fields=id,date,slug,title,excerpt,author,featured_media,jetpack_featured_media_url,categories,_links,_embedded");
   const posts: Post[] = await response.json();
+  return posts;
+}
+
+export default async function Home() {
+  const posts: Post[] = await fetchPosts();
   const featuredPosts = posts.slice(0, 5);
   return (
     <>
