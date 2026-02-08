c59a5c0518b4806c67cbd98efd3226f9fb89724f
Miguel Ángel <gomezbenitezmiguelangel@outlook.com>
2026-02-08 20:04:08 +0100

    chore: remove pre-election content restriction for Aragon 2026 page.

--- app/elecciones/aragon/2026/page.tsx
+++ app/elecciones/aragon/2026/page.tsx
@@ -14,16 +14,6 @@ export default async function Elecciones() {
   const date = new Date().getHours();
 
   const timeOpen = new Date().getHours() >= 20;
-
-  if(Date.now() < new Date("2026-02-08T20:00:00").getTime()) {
-    return (
-      <>
-        <Header />
-        <h1 className="text-3xl text-center m-10 font-black">El escrutinio todavia no ha empezado</h1>
-        <Avances />
-      </>
-    );
-  }
   return (
     <>
       <Header />
