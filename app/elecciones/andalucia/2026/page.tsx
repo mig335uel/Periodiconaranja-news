47c0520e1429c7b692148b6f4a3452ebd09a352a
Miguel Ángel <gomezbenitezmiguelangel@outlook.com>
2026-05-05 15:43:08 +0200

    feat: restrict access to Andalusian elections page until May 17th for non-admin users

--- app/elecciones/andalucia/2026/page.tsx
+++ app/elecciones/andalucia/2026/page.tsx
@@ -2,6 +2,10 @@ import EscrutinioWidget from "@/components/ui/Escrutinio";
 import Header from "../../../Header";
 import Avances from "@/components/Elecciones/Avances";
 import EscrutinioTotal from "@/components/Elecciones/EscrutinioTotal";
+import { User } from "@/Types/Account";
+import {createClient} from "@/lib/supabase/server";
+
+
 
 export async function generateMetadata() {
   return {
@@ -10,9 +14,38 @@ export async function generateMetadata() {
   };
 }
 
+async function getCurrentUser() {
+  const supabase = await createClient();
+  const { data: dataAuth, error: errorAuth } = await supabase.auth.getUser();
+
+
+  const { data, error } = await supabase.from('users').select('*').eq('id', dataAuth.user?.id).single();
+  return data;
+}
+
+
+
 export default async function Elecciones() {
 
-  const date = new Date().getHours();
+  const fechaActual = new Date(Date.now());
+  console.log(fechaActual);
+  const fechadeApertura = new Date("2026-05-17T20:00:00");
+  const user = await getCurrentUser();
+
+  if ((!user || user.role === 'viewer') && fechaActual < fechadeApertura) {
+    return (
+      <>
+        <Header />
+        <div className="flex flex-col justify-center items-center h-screen">
+          <h2 className="text-2xl font-bold text-gray-800">
+            Elecciones Andalucía 2026
+          </h2>
+          <br />
+          <p className="text-gray-600">Esta pagina será habilitada el 17 de Mayo a las 08:00</p>
+        </div>
+      </>
+    );
+  }
 
   return (
     <>
