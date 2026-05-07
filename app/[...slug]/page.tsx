a7a2312f16b74fb97168c9812bae2e86c92e34c4
Miguel Ángel <gomezbenitezmiguelangel@outlook.com>
2026-05-07 18:17:41 +0200

    fix: remove unnecessary _embed parameter from post fetch request

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -19,7 +19,7 @@ interface Props {
 async function fetchPost(slug: string): Promise<Post | null> {
   try {
     const res = await fetch(
-      `${process.env.CMS_URL}/wp-json/wp/v2/posts?slug=${slug}&_embed`,
+      `${process.env.CMS_URL}/wp-json/wp/v2/posts?slug=${slug}`,
       { next: { tags: [`post-${slug}`, 'all-posts'] } } // Caché permanente. Solo se actualizará cuando WordPress avise.
     );
     if (!res.ok) return null;
